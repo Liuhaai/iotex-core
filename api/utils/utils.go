deccb4d65ffb3ceda3e821612b2088987d3ec677
Liuhaai <haixiang@iotex.io>
2022-04-06 18:03:01 -0700

    add comment

--- api/utils/utils.go
+++ api/utils/utils.go
@@ -2,6 +2,7 @@ package apiutils
 
 // TODO: move this code to web3server.go after issue#3248 is solved
 type (
+	// JSONWriter is writer for web3 request
 	JSONWriter struct {
 		Write       func(interface{}) error
 		IsWriteOnce bool

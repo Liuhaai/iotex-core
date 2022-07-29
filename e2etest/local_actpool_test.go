19b2be1434e86bff2a7ebfef0514fac3de605d9a
Liuhaai <haixiang@iotex.io>
2022-07-29 16:39:11 -0700

    update actpool test

--- e2etest/local_actpool_test.go
+++ e2etest/local_actpool_test.go
@@ -8,20 +8,23 @@ package e2etest
 
 import (
 	"context"
-	"fmt"
 	"math/big"
+	"math/rand"
 	"testing"
 	"time"
 
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
 	"google.golang.org/protobuf/proto"
 
 	"github.com/iotexproject/go-pkgs/crypto"
+	"github.com/iotexproject/iotex-proto/golang/iotextypes"
 
 	"github.com/iotexproject/iotex-core/action"
 	"github.com/iotexproject/iotex-core/config"
 	"github.com/iotexproject/iotex-core/p2p"
+	"github.com/iotexproject/iotex-core/pkg/log"
 	"github.com/iotexproject/iotex-core/server/itx"
 	"github.com/iotexproject/iotex-core/test/identityset"
 	"github.com/iotexproject/iotex-core/testutil"
@@ -29,57 +32,25 @@ import (
 
 func TestLocalActPool(t *testing.T) {
 	require := require.New(t)
-
-	cfg, err := newActPoolConfig(t)
-	require.NoError(err)
-
-	// create server
 	ctx := context.Background()
-	svr, err := itx.NewServer(cfg)
-	require.NoError(err)
 
+	cfg := newActPoolConfig(require)
+	svr, cli, err := createServerAndClient(require, cfg)
+	require.NoError(err)
 	chainID := cfg.Chain.ID
-	fmt.Println("server start")
-	require.NoError(svr.Start(ctx))
-	fmt.Println("server started")
-	require.NotNil(svr.ChainService(chainID).ActionPool())
-
-	// create client
-	cfg, err = newActPoolConfig(t)
-	require.NoError(err)
-	addrs, err := svr.P2PAgent().Self()
-	require.NoError(err)
-	cfg.Network.BootstrapNodes = []string{validNetworkAddr(addrs)}
-	cli := p2p.NewAgent(
-		cfg.Network,
-		cfg.Chain.ID,
-		cfg.Genesis.Hash(),
-		func(_ context.Context, _ uint32, _ string, _ proto.Message) {
-
-		},
-		func(_ context.Context, _ uint32, _ peer.AddrInfo, _ proto.Message) {
-
-		},
-	)
-	require.NotNil(cli)
-	require.NoError(cli.Start(ctx))
-	fmt.Println("p2p agent started")
-
 	defer func() {
 		require.NoError(cli.Stop(ctx))
 		require.NoError(svr.Stop(ctx))
 	}()
 
-	// Create three valid actions from "from" to "to"
+	// Wait until server receives the 1st action
 	tsf1, err := action.SignedTransfer(identityset.Address(0).String(), identityset.PrivateKey(1), 1, big.NewInt(1), []byte{}, uint64(100000), big.NewInt(0))
 	require.NoError(err)
-	// Wait until server receives the 1st action
-	require.NoError(testutil.WaitUntil(100*time.Millisecond, 60*time.Second, func() (bool, error) {
+	require.NoError(testutil.WaitUntil(100*time.Millisecond, 10*time.Second, func() (bool, error) {
 		require.NoError(cli.BroadcastOutbound(ctx, tsf1.Proto()))
 		acts := svr.ChainService(chainID).ActionPool().PendingActionMap()
 		return lenPendingActionMap(acts) == 1, nil
 	}))
-	fmt.Println("1")
 
 	tsf2, err := action.SignedTransfer(identityset.Address(1).String(), identityset.PrivateKey(1), 2, big.NewInt(3), []byte{}, uint64(100000), big.NewInt(0))
 	require.NoError(err)
@@ -88,7 +59,7 @@ func TestLocalActPool(t *testing.T) {
 	// Create contract
 	exec4, err := action.SignedExecution(action.EmptyAddress, identityset.PrivateKey(1), 4, big.NewInt(0), uint64(120000), big.NewInt(10), []byte{})
 	require.NoError(err)
-	// Create three invalid actions from "from" to "to"
+	// Create one invalid action
 	tsf5, err := action.SignedTransfer(identityset.Address(0).String(), identityset.PrivateKey(1), 2, big.NewInt(3), []byte{}, uint64(100000), big.NewInt(0))
 	require.NoError(err)
 
@@ -97,34 +68,78 @@ func TestLocalActPool(t *testing.T) {
 	require.NoError(cli.BroadcastOutbound(ctx, exec4.Proto()))
 	require.NoError(cli.BroadcastOutbound(ctx, tsf5.Proto()))
 
-	fmt.Println("2")
 	// Wait until server receives all the transfers
-	require.NoError(testutil.WaitUntil(100*time.Millisecond, 60*time.Second, func() (bool, error) {
+	require.NoError(testutil.WaitUntil(100*time.Millisecond, 10*time.Second, func() (bool, error) {
 		acts := svr.ChainService(chainID).ActionPool().PendingActionMap()
-		// 3 valid transfers and 1 valid execution
+		// 3 valid transfers and 1 valid execution in total
 		return lenPendingActionMap(acts) == 4, nil
 	}))
-	fmt.Println("3")
+}
+
+func BenchmarkActpool(b *testing.B) {
+	require := require.New(b)
+	ctx := context.Background()
+
+	cfg := newActPoolConfig(require)
+	svr, cli, err := createServerAndClient(require, cfg)
+	require.NoError(err)
+	chainID := cfg.Chain.ID
+	defer func() {
+		require.NoError(cli.Stop(ctx))
+		require.NoError(svr.Stop(ctx))
+	}()
+
+	// Wait until server receives the 1st action
+	tsf1, err := action.SignedTransfer(identityset.Address(0).String(), identityset.PrivateKey(1), 1, big.NewInt(1), []byte{}, uint64(100000), big.NewInt(0))
+	require.NoError(err)
+	require.NoError(testutil.WaitUntil(100*time.Millisecond, 10*time.Second, func() (bool, error) {
+		require.NoError(cli.BroadcastOutbound(ctx, tsf1.Proto()))
+		acts := svr.ChainService(chainID).ActionPool().PendingActionMap()
+		return lenPendingActionMap(acts) == 1, nil
+	}))
+
+	// Generate msgs to be sent
+	var (
+		totalActs = 2
+		nonceMap  = make(map[string]int)
+		msgs      = make([]*iotextypes.Action, 0, totalActs)
+	)
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < totalActs; i++ {
+		senderIdx := rand.Intn(identityset.Size())
+		receiverIdx := rand.Intn(identityset.Size())
+		sender := identityset.Address(senderIdx)
+		nonceMap[sender.Hex()]++
+		tsf, err := action.SignedTransfer(identityset.Address(receiverIdx).String(), identityset.PrivateKey(senderIdx), uint64(nonceMap[sender.Hex()]), big.NewInt(0), []byte{}, uint64(10000), big.NewInt(0))
+		require.NoError(err)
+		msgs = append(msgs, tsf.Proto())
+	}
+
+	for i := range msgs {
+		cli.BroadcastOutbound(ctx, msgs[i])
+	}
+
+	require.NoError(testutil.WaitUntil(100*time.Millisecond, 3*time.Second, func() (bool, error) {
+		acts := svr.ChainService(chainID).ActionPool().PendingActionMap()
+
+		res := lenPendingActionMap(acts)
+		log.L().Info("asd", zap.Int("len", res))
+		return res == 3, nil
+	}))
 
 }
 
-func TestPressureActPool(t *testing.T) {
-	require := require.New(t)
-
-	cfg, err := newActPoolConfig(t)
-	require.NoError(err)
-
+func createServerAndClient(require *require.Assertions, cfg config.Config) (*itx.Server, p2p.Agent, error) {
 	// create server
 	ctx := context.Background()
 	svr, err := itx.NewServer(cfg)
 	require.NoError(err)
-	require.NoError(svr.Start(ctx))
 	chainID := cfg.Chain.ID
+	require.NoError(svr.Start(ctx))
 	require.NotNil(svr.ChainService(chainID).ActionPool())
 
 	// create client
-	cfg, err = newActPoolConfig(t)
-	require.NoError(err)
+	cfg = newActPoolConfig(require)
 	addrs, err := svr.P2PAgent().Self()
 	require.NoError(err)
 	cfg.Network.BootstrapNodes = []string{validNetworkAddr(addrs)}
@@ -132,56 +147,23 @@ func TestPressureActPool(t *testing.T) {
 		cfg.Network,
 		cfg.Chain.ID,
 		cfg.Genesis.Hash(),
-		func(_ context.Context, _ uint32, _ string, _ proto.Message) {
-
-		},
-		func(_ context.Context, _ uint32, _ peer.AddrInfo, _ proto.Message) {
-
-		},
+		func(_ context.Context, _ uint32, _ string, _ proto.Message) {},
+		func(_ context.Context, _ uint32, _ peer.AddrInfo, _ proto.Message) {},
 	)
-	require.NotNil(cli)
 	require.NoError(cli.Start(ctx))
 
-	defer func() {
-		require.NoError(cli.Stop(ctx))
-		require.NoError(svr.Stop(ctx))
-	}()
-
-	tsf, err := action.SignedTransfer(identityset.Address(0).String(), identityset.PrivateKey(1), 1, big.NewInt(int64(0)), []byte{}, uint64(100000), big.NewInt(0))
-	require.NoError(err)
-	// Wait until server receives the 1st action
-	require.NoError(testutil.WaitUntil(100*time.Millisecond, 60*time.Second, func() (bool, error) {
-		require.NoError(cli.BroadcastOutbound(ctx, tsf.Proto()))
-		acts := svr.ChainService(chainID).ActionPool().PendingActionMap()
-		return lenPendingActionMap(acts) == 1, nil
-	}))
-
-	// Broadcast has rate limit at 300
-	for i := 2; i <= 250; i++ {
-		tsf, err := action.SignedTransfer(identityset.Address(0).String(), identityset.PrivateKey(1), uint64(i), big.NewInt(int64(i)), []byte{}, uint64(100000), big.NewInt(0))
-		require.NoError(err)
-		require.NoError(cli.BroadcastOutbound(ctx, tsf.Proto()))
-	}
-
-	// Wait until committed blocks contain all broadcasted actions
-	err = testutil.WaitUntil(100*time.Millisecond, 60*time.Second, func() (bool, error) {
-		acts := svr.ChainService(chainID).ActionPool().PendingActionMap()
-		return lenPendingActionMap(acts) == 250, nil
-	})
-	require.NoError(err)
+	return svr, cli, nil
 }
 
-func newActPoolConfig(t *testing.T) (config.Config, error) {
-	r := require.New(t)
-
+func newActPoolConfig(require *require.Assertions) config.Config {
 	cfg := config.Default
 
 	testTriePath, err := testutil.PathOfTempFile("trie")
-	r.NoError(err)
+	require.NoError(err)
 	testDBPath, err := testutil.PathOfTempFile("db")
-	r.NoError(err)
+	require.NoError(err)
 	testIndexPath, err := testutil.PathOfTempFile("index")
-	r.NoError(err)
+	require.NoError(err)
 
 	defer func() {
 		testutil.CleanupPath(testTriePath)
@@ -198,9 +180,7 @@ func newActPoolConfig(t *testing.T) (config.Config, error) {
 	cfg.Network.Port = testutil.RandomPort()
 
 	sk, err := crypto.GenerateKey()
-	if err != nil {
-		return config.Config{}, err
-	}
+	require.NoError(err)
 	cfg.Chain.ProducerPrivKey = sk.HexString()
-	return cfg, nil
+	return cfg
 }
